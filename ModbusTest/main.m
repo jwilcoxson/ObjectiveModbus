23314e49017d9cede5082ce374b233e5683edd50
Joe Wilcoxson <joe.wilcoxson@gmail.com>
2016-05-30 10:16:02 -0400

    Initial Commit

---
+++ ModbusTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ModbusTest
+//
+//  Created by Joe Wilcoxson on 5/30/16.
+//  Copyright © 2016 Joe Wilcoxson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
